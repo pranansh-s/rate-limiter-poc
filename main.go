350f4ad6dccf25051e75624cbd764aa61ba4017b
pranansh-s <prananshsingh@gmail.com>
2026-07-09 19:14:49 +0530

    basic server setup

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("HELLO")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("pong\n"))
+	})
+	log.Println("listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux))
 }
